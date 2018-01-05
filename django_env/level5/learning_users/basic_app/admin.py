38c194320d0a3335683b93eee9be8b938fcd44dd
Todd Riemenschneider <twr14152@gmail.com>
2018-01-05 16:24:56 +0000

    Update admin.py

--- django_env/level5/learning_users/basic_app/admin.py
+++ django_env/level5/learning_users/basic_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from basic_app.models import UserProfileInfo
+from basic_app.models import UserProfileInfo, User
 # Register your models here.
 admin.site.register(UserProfileInfo)

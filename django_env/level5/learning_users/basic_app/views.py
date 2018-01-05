2a719ae198c2c5bd345ccc4a8e43877202f6f70e
Todd Riemenschneider <twr14152@gmail.com>
2018-01-05 18:57:17 -0500

    Update views.py

--- django_env/level5/learning_users/basic_app/views.py
+++ django_env/level5/learning_users/basic_app/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from basic_app.forms import UserForm, UserProfileInfoForm
 # Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse 
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+
 def index(request):
 	return render(request,'basic_app/index.html')
+
+@login_required
+def special(request):
+	return HttpResponse("You are logged in, Nice!")
+
+@login_required
+def user_logout(request):
+	lougout(request)
+	return HttpResponseRedirect(reverse('index'))
+	
+	
+	
 	
 def register(request):
 	
@@ -41,3 +59,28 @@ def register(request):
 							 'registered':registered})
 							 		
 		
+
+def user_login(request):
+	
+	if request.method == "POST":
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		
+		user = authenticate(username=username,password=password)
+		
+		
+		if user:
+			if user.is_active:
+				login(requests, user)
+				return HttpResponseRedirect(reverse('index'))
+				
+				
+			else:
+				HttpResponse("ACCOUNT NOT ACTIVE")
+				
+		else:
+			print("Someone tried to login and failed!")
+			print("Username: {} and password {}".format(username,password))
+			return HttpResponse("invalid login details supplied!")
+	else:
+		return render(request,'basic_app/login.html',{})

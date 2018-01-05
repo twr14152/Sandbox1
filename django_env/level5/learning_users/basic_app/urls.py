ff5980fc82910a65c548b5a43421bcacb5860d47
Todd Riemenschneider <twr14152@gmail.com>
2018-01-05 18:56:29 -0500

    Update urls.py

--- django_env/level5/learning_users/basic_app/urls.py
+++ django_env/level5/learning_users/basic_app/urls.py
@@ -5,6 +5,6 @@ from basic_app import views
 app_name = 'basic_app'
 
 urlpatterns = [
-    path('register',views.register,name='register')
-    
-    ]
+    path('register',views.register,name='register'),
+    path('user_login/',views.user_login,name='user_login'),
+]
